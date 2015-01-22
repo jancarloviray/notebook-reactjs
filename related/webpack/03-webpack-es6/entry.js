061488626d2709715b77a150b15a040c7ccd4c7e
Jan Carlo Viray <virayjancarlo@yahoo.com>
2015-01-21 23:14:15 -0800

    Added native es6 promises example

--- related/webpack/03-webpack-es6/entry.js
+++ related/webpack/03-webpack-es6/entry.js
@@ -13,3 +13,10 @@ class Employee extends Person {
 
 var jc = new Employee();
 console.log(jc.info());
+
+// promises example
+var p1 = new Promise((resolve, reject) => setTimeout(resolve, 400, "one"));
+var p2 = new Promise((resolve, reject) => setTimeout(resolve, 200, "two"));
+Promise.all([p1, p2]).then(function(value) {
+    console.log(value); //one, two
+});
