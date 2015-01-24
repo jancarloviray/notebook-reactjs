25fea1638ccd201a4673aaec0f3870e699e5544d
Jan Carlo Viray <virayjancarlo@yahoo.com>
2015-01-24 02:55:38 -0800

    Added Sample .jsx files

---
+++ related/webpack/04-reactjs/app.jsx
@@ -0,0 +1,11 @@
+/** @jsx Reach.DOM */
+
+'use strict'
+
+var React = require('react');
+var Hello = require('./Hello');
+
+React.render(
+    <Hello />,
+    document.body
+);
---
+++ related/webpack/04-reactjs/hello.jsx
@@ -0,0 +1,10 @@
+'use strict'
+
+var Reach = require('react');
+
+module.export = React.createClass({
+    displayName: 'HelloReact',
+    render: function(){
+        return <div>Hello React</div>
+    }
+});
